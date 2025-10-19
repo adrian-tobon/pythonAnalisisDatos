067cc491ee5fc0551dfad98e96637279269f72b4
ctobonpe <ctobonpe@gmail.com>
2025-10-19 12:21:42 -0500

    graficos dispersion, regresion lineal y  multiple

--- AnalisisCorrelacionRegresion.py
+++ AnalisisCorrelacionRegresion.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
 ##########Analisis de correlacion y regresion##########
 
@@ -47,7 +49,7 @@ sns.pairplot(df_sales_data_correlated[['Price Each','Quantity Ordered']])
 plt.show()
 '''
 #graficos de dispersion
-
+'''
 plt.figure(figsize=(5,3))
 #plt.scatter(df_sales_data_correlated['Price Each'],df_sales_data_correlated['Quantity Ordered'], alpha=0.5)
 sns.scatterplot(df_sales_data_correlated,x='Price Each',y='Quantity Ordered')
@@ -55,3 +57,89 @@ plt.title('Dispersion Correlacion')
 plt.xlabel('Price Each')
 plt.ylabel('Quantity Ordered')
 plt.show()
+'''
+#agrupacion por datos categoricos
+'''
+plt.figure(figsize=(5,3))
+#plt.scatter(df_sales_data_correlated['Price Each'],df_sales_data_correlated['Quantity Ordered'], alpha=0.5)
+sns.scatterplot(df_sales_data_correlated,x='Price Each',y='Quantity Ordered',hue='Product',palette='viridis')
+plt.title('Dispersion Correlacion')
+plt.xlabel('Price Each')
+plt.ylabel('Quantity Ordered')
+#plt.legend(bbox_to_anchor=(1.05,1),loc='upper left')
+plt.show()
+'''
+
+#dispersion con regresion lineal
+'''
+plt.figure(figsize=(5,3))
+#plt.scatter(df_sales_data_correlated['Price Each'],df_sales_data_correlated['Quantity Ordered'], alpha=0.5)
+#sns.scatterplot(df_sales_data_correlated,x='Price Each',y='Quantity Ordered',hue='Product',palette='viridis', alpha=0.5, s=100, edgecolor='k')
+sns.regplot(df_sales_data_correlated,x='Price Each',y='Quantity Ordered',scatter_kws={'alpha':0.5,'s':100,'edgecolor':'k'},line_kws={'color':'red'})
+plt.title('Dispersion Correlacion')
+plt.xlabel('Price Each')
+plt.ylabel('Quantity Ordered')
+plt.grid(True,linestyle='--',alpha=0.5)
+#plt.legend(bbox_to_anchor=(1.05,1),loc='upper left')
+plt.show()
+'''
+
+# regrsion lineal y multiple
+
+# regresion lineal simple
+
+x = df_sales_data_correlated[['Price Each']] #variable independiente
+y = df_sales_data_correlated[['Quantity Ordered']] #variable dependiente
+
+model = LinearRegression()
+model.fit(x,y)
+
+y_pred = model.predict(x)
+
+'''
+plt.figure(figsize=(5,3))
+sns.scatterplot(df_sales_data_correlated,x='Price Each',y='Quantity Ordered', alpha=0.5)
+plt.plot(x,y_pred,color='red',linewidth=2)
+plt.title('Regresion Lineal: Price Each vs. Quantity Ordered')
+plt.xlabel('Price Each')
+plt.ylabel('Quantity Ordered')
+plt.grid(True,linestyle='--',alpha=0.5)
+plt.show()
+'''
+
+r_squared = model.score(x,y)
+print(f"R²(coeficiente de determinacion) del modelo = {r_squared}")
+
+#regresion multiple
+
+df_sales_data_correlated['Order Month'] = pd.to_datetime(df_sales_data_correlated['Order Date'],errors='coerce').dt.month
+print(df_sales_data_correlated)
+
+x_multiple = df_sales_data_correlated[['Price Each','Order Month']]
+#y = df_sales_data_correlated['Quantity Ordered'] # ya se declaro previasmente
+
+model_multiple = LinearRegression()
+model_multiple.fit(x_multiple,y)
+
+print(f"Coeficientes: {model_multiple.coef_}")
+print(f"interseccion: {model_multiple.intercept_}")
+
+r_squared_multiple = model_multiple.score(x_multiple,y)
+print(f"R²(coeficiente de determinacion) del modelo multiple = {r_squared_multiple}")
+
+#residuales
+residuals = y - model_multiple.predict(x_multiple)
+print(type(model_multiple.predict(x_multiple)))
+print(type(residuals))
+
+#no me funciona el grafico
+'''
+plt.figure(figsize=(5,3))
+sns.scatterplot(x=model_multiple.predict(x_multiple), y=residuals)
+plt.axhline(0,color='red',linestyle='--')
+plt.title('Grafico Residuales')
+plt.xlabel('Predicciones')
+plt.ylabel('Residuos')
+plt.grid(True,linestyle='--',alpha=0.5)
+plt.show()
+'''
